e2008746bd861faab4cb0068a8f94003201a4a8b
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-02-25 23:02:59 +0400

    add db

--- library-api/routes/booksRouter.js
+++ library-api/routes/booksRouter.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
+import { Book } from "../models/book.js";
 
 export const booksRouter = Router();
+
+booksRouter.get("/", async (req, res, next) => {
+  try {
+    const books = await Book.findAll();
+    res.json(books);
+  } catch (e) {
+    console.error(`booksRouter get error: ${e.message}`);
+    next(e);
+  }
+});
 
 booksRouter.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const book = { id };
+    const book = await Book.findByPk(id);
     res.json(book);
   } catch (e) {
     console.error(`booksRouter get error: ${e.message}`);
@@ -16,8 +27,17 @@ booksRouter.get("/:id", async (req, res, next) => {
 booksRouter.post("/", async (req, res, next) => {
   const { title, author } = req.body;
   try {
-    const book = { title, author };
-    res.json(book);
+    const savedBook = await Book.findOne({ where: { title, author } });
+    if (savedBook) {
+      const book = await Book.update(
+        { title, author, count: savedBook.count + 1 },
+        { where: { id: savedBook.id } }
+      );
+      res.json(book);
+    } else {
+      const book = await Book.create({ title, author, count: 1 });
+      res.json(book);
+    }
   } catch (e) {
     console.error(`booksRouter post error: ${e.message}`);
     next(e);
@@ -26,8 +46,10 @@ booksRouter.post("/", async (req, res, next) => {
 
 booksRouter.put("/:id", async (req, res, next) => {
   const { id } = req.params;
+  const { title, author } = req.body;
+  console.log({ id, title, author });
   try {
-    const book = { id };
+    const book = await Book.update({ title, author }, { where: { id } });
     res.json(book);
   } catch (e) {
     console.error(`booksRouter put error: ${e.message}`);
@@ -35,10 +57,10 @@ booksRouter.put("/:id", async (req, res, next) => {
   }
 });
 
-booksRouter.put("/:id", async (req, res, next) => {
+booksRouter.delete("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const book = { id };
+    const book = await Book.destroy({ where: { id } });
     res.json(book);
   } catch (e) {
     console.error(`booksRouter delete error: ${e.message}`);
