b4b9622ab65a304ec6b5af8e0fd861cd307081cf
Ivan Arkaev <75647984+ha308ing@users.noreply.github.com>
2024-02-25 12:43:12 +0400

    add user's link

--- rss-feed/index.js
+++ rss-feed/index.js
@@ -1,11 +1,15 @@
 import Parser from "rss-parser";
+import promptModule from "prompt-sync";
 
 const parser = new Parser();
+const prompt = promptModule({ sigint: true });
 
 const urls = [
   "https://www.bonappetit.com/feed/recipes-rss-feed/rss",
   "https://www.reddit.com/r/recipes/.rss",
 ];
+
+const customItems = [];
 
 // setInterval(main, 5000);
 main();
@@ -30,7 +34,10 @@ function aggregate(responses, feedItems) {
 }
 
 function print(feedItems) {
+  const res = prompt("Add item (name,url): ");
+  const [title, link] = res.split(",");
+  if (![title, link].includes(undefined)) customItems.push({ title, link });
   console.clear();
-  console.table(feedItems);
+  console.table(feedItems.concat(customItems));
   console.log(`last updated at ${new Date().toUTCString()}`);
 }
